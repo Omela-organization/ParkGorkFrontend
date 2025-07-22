7c9f76739feafb243615586757c30a78d2041a14
shengnan <pelevin1111@gmail.com>
2025-07-22 13:52:13 +0300

    Fix env names

--- src/config/settings.js
+++ src/config/settings.js
@@ -7,5 +7,5 @@ export const API_BASE_ECO = withPort(import.meta.env.VITE_API_ECO_PORT)
 export const API_BASE_ENTERTAINMENT = withPort(import.meta.env.VITE_API_ENTERTAINMENT_PORT)
 export const API_BASE_STORAGE = withPort(import.meta.env.VITE_API_STORAGE_PORT)
 
-export const API_PREFIX = import.meta.env.API_PREFIX
-export const REQUEST_TIMEOUT = import.meta.env.REQUEST_TIMEOUT
+export const API_PREFIX = import.meta.env.VITE_API_PREFIX
+export const REQUEST_TIMEOUT = import.meta.env.VITE_REQUEST_TIMEOUT
